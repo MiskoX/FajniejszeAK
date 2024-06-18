d7581a0b1c26db029cfbee8ef51327bfe0f91251
MiskoX <michal.kalwak@gmail.com>
2024-06-18 22:10:35 +0200

    Wersja 1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Quiz from "./pages/Quiz"
+
+function App() {
+  return (
+    <div className="App">
+      <Quiz/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Quiz.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import questionsData from "../data/questions.json";
+
+function Quiz() {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [procent, setProcent] = useState(0);
+  const [answeredQuestions, setAnsweredQuestions] = useState(0);
+  const [allOfQuestion, setAllOfQuestion] = useState(0);
+  const [questionBoxClass, setQuestionBoxClass] = useState(null); // Stan dla klasy CSS
+  const [stateOfQuestion, setStateOfQuestion] = useState(null);
+
+  // Efekt do pobrania danych z pliku JSON
+  useEffect(() => {
+    setQuestions(questionsData);
+    setAllOfQuestion(questionsData.length);
+    setProcent(0);
+  }, []);
+
+  const handleAnswer = (answer) => {
+    debugger;
+    const answerQue = answeredQuestions + 1;
+    var correctAns = correctAnswers;
+    // Sprawdzenie poprawności odpowiedzi
+    if (answer === questions[currentQuestionIndex].Answer) {
+      correctAns = correctAns + 1;
+      setQuestionBoxClass("correct"); // Ustawienie klasy CSS dla poprawnej odpowiedzi
+      setCorrectAnswers(correctAns); // Zwiększenie liczby poprawnych odpowiedzi
+    } else {
+      setQuestionBoxClass("incorrect"); // Ustawienie klasy CSS dla niepoprawnej odpowiedzi
+    }
+
+    // Obliczenie procentu postępu
+    const percentage = Math.round((correctAns / answerQue) * 100);
+    setProcent(percentage);
+    setAnsweredQuestions(answerQue);
+    setStateOfQuestion("1");
+  };
+
+  const nextQuestion = () => {
+    if (currentQuestionIndex + 1 < allOfQuestion) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      // Tutaj możesz dodać logikę zakończenia quizu
+      // Na przykład wyświetlić wynik końcowy lub zresetować stan quizu
+      console.log("Koniec quizu");
+    }
+    setQuestionBoxClass(null);
+    setStateOfQuestion(null);
+  };
+
+  return (
+    <div className="Quiz">
+      <div className="Navbar">
+        <p>
+          Liczba pytań: {currentQuestionIndex + 1}/{allOfQuestion}
+        </p>
+        <p>Liczba Procent: {procent}%</p>
+      </div>
+      <div className={`QuestionBox ${questionBoxClass}`}>
+        {questions.length > 0 && (
+          <div className="Question">
+            <div>Pytanie:</div>
+            <p>{questions[currentQuestionIndex].Question}</p>
+          </div>
+        )}
+        {stateOfQuestion ? (
+          <>
+            <div className="Description">
+              Opis:
+              <p> {questions[currentQuestionIndex].Description}</p>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      {stateOfQuestion ? (
+        <div className="SingleButton">
+          <div className="Button">
+            <button onClick={() => nextQuestion()}>Następne pytanie</button>
+          </div>
+        </div>
+      ) : (
+        <div className="Buttons">
+          <div className="Button">
+            <button onClick={() => handleAnswer(true)}>Prawda</button>
+          </div>
+          <div className="Button">
+            <button onClick={() => handleAnswer(false)}>Fałsz</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
