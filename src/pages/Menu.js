76eea9fd95ae691cec5369ca1256645f5e743f6f
MiskoX <michal.kalwak@gmail.com>
2024-06-25 01:45:44 +0200

    .

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -5,13 +5,23 @@ import Logo from "../assets/logo.png";
 function Menu() {
   return (
     <div className="Menu">
-      <div className="Logo">
-        <img src={Logo}></img>
-      </div>
+      <Link
+        to="/posters"
+        style={{
+          padding: 0,
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          width: "100%",
+          cursor: "default",
+        }}
+      >
+        <div className="Logo">
+          <img src={Logo}></img>
+        </div>
+      </Link>
       <Link to="/classic">Pytania po kolei</Link>
       <Link to="/random">Pytania losowe</Link>
       <Link to="/category">Pytania z kategorii</Link>
-      <Link to="/posters">Hmm</Link>
     </div>
   );
 }
